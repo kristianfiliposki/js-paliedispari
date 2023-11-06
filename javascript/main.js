4b44d093b6c26e09db844e3972c6dcebce5408bf
kristiafiliposki <filiposki.kristian1112@gmail.com>
2023-11-06 17:30:19 +0100

    0.2

--- javascript/main.js
+++ javascript/main.js
@@ -1,20 +1,32 @@
 const input=document.getElementById("input");
 const invio=document.getElementById("invio");
 let output=document.getElementById("output");
+let output2=document.getElementById("output2");
 
 invio.addEventListener("click",function(){
     
     let words=input.value;
-    output.innerHTML+=words
-    for (let i = words.length; i >= 0; i--) {
-        const Ncharacter = words[i];
+    output.innerHTML=words;
+
+    let i = words.length
+    while ( i >= 0 ) {
+        let Ncharacter = words[i];
+        i--;
         console.log(Ncharacter)
-    }
+        output2.innerHTML+=Ncharacter
+    }   
+
+/*     function Palindroma (){
+        
+        if (output==output2) {
+            console.log("è palindroma")
+        }
+        else(
+            console.log("Non è palindroma")
+        )
+        
+     }  */
  })
 
 
- function Palindroma (){
-    
-    
-
- }
+ 
